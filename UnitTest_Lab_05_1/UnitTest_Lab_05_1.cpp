7911511848fdf2eba96040eebfd48c687b1d49cb
Glit <vpad.lazar.04@gmail.com>
2022-11-02 12:44:34 +0200

    test branch commit

---
+++ UnitTest_Lab_05_1/UnitTest_Lab_05_1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_05_1/Lab_05_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab051
+{
+	TEST_CLASS(UnitTestLab051)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double h;
+			h = g(5, 3);	
+			Assert::AreEqual(h, 49.0);
+		}
+	};
+}
